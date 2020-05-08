d5f4793e9f3f3b20b9f2f5613479bcbbea7d6461
mbXdev <63065904+mbXdev@users.noreply.github.com>
2020-05-08 19:39:53 +0200

    Added TuringTestView class

---
+++ TuringTestView.php
@@ -0,0 +1,62 @@
+<?php
+    /* utilisation:
+        TuringTestView::prepare($formAction, $infoMessageHtml, $imageSRC, $useToken);
+        TuringTestView::showView();
+    */
+
+    require_once 'TuringTestUsingDatabase.php';
+
+    class TuringTestView
+    {
+        public static function prepare(
+            string $formAction, string $infoMessageHtml, string $imageSRC, bool $useToken) {
+
+            self::$formAction = $formAction;
+            self::$infoMessageHtml = $infoMessageHtml;
+            self::$imageSRC = $imageSRC;
+            self::$useToken = $useToken;
+        }
+
+        protected static $formAction;
+        protected static $infoMessageHtml;
+        protected static $imageSRC;
+        protected static $useToken;
+
+        public static function showView()
+        {
+            $imageTag = '';
+            $tokenInput = '';
+            if (self::$useToken) {
+                // Récupération/Création du jeton
+                $token = !empty($_POST['token']) ? $_POST['token'] : TuringTestUsingDatabase::newToken();
+                $imageTag = '<img src="'.self::$imageSRC.'?token='.$token.'" style="width: 130px;">';
+                $tokenInput = '<input type="hidden" name="token" value="'.$token.'" />';
+            }
+            
+            // Préparation du formulaire
+            if (empty($_POST['turingTest_answer'])) {
+                $content = '';
+                $content .= self::$infoMessageHtml . '
+                    <form action="' . self::$formAction . '" method="post">
+                    <br/>
+                    <label for="turingTest_answer">Veuillez résoudre le problème ci-dessous</label>
+                    ' . $imageTag . '
+                    <br/>
+                    <input type="text" name="turingTest_answer" id="turingTest_answer" size="10" required/>
+                    <br/>
+                    ' . $tokenInput;
+                // Renvoyer les données du formulaire précédent
+                foreach ($_POST as $key => $value) {
+                    if ($key == 'token' || $key == 'turingTest_answer') continue;
+                    $content .= '<input type="hidden" name="' . $key . '" value="' . $value . '" />';
+                }
+                $content .= '<input type="submit" value="Vérifier">
+                    </form>';
+                // Affichage du formulaire permettant de passer le test
+                echo $content;
+            }
+            else {
+                // La réponse au test doit être capturé plus haut.
+            }
+        }
+    }
